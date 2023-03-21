262fde51c37866bcc63d1a0b8455d58642e675bc
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-03-21 17:34:22 -0400

    created main structure

---
+++ src/components/Game.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div className="gameContainer"></div>
+  )
+}
+
+export default Game
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Aim Trainer</h1>
+    </div>
+  )
+}
+
+export default Header
